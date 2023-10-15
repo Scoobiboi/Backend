1628ed5ca2e4e8c556524d48c684e765e8fedd72
Scoobiboi <lukeanthonyteuamcleod@gmail.com>
2023-10-15 17:29:14 +1300

    Testing the dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getConnectionInfo } from '../config/db';
+import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
+  const dbStatus = await getConnectionInfo();
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
+  console.log(`Database connection status: ${dbStatus}`);
+  console.log(`Database connection NAME: ${process.env.DB_USER}`);
   await app.listen(3000);
 }
 bootstrap();
