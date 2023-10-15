39105ed4421d9e82eabfe396bc13acf78e7883a9
Scoobiboi <lukeanthonyteuamcleod@gmail.com>
2023-10-15 18:22:24 +1300

    Checking APIS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import {getArticles} from '../config/db'
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/articles')
+  async getArticles() {
+    const articles = await getArticles();
+    return articles;
+  }
+
 }
